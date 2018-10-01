81389c096058c79107fd62e7043f4b0357689088
Kyle Sargent <sargentakyle@gmail.com>
2018-10-01 16:13:53 -0500

    SpaceWar game created using python3

---
+++ Python3/SpaceWar/SpaceWar.py
@@ -0,0 +1,241 @@
+import winsound
+import random
+import turtle
+import time
+
+turtle.fd(0)
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1)
+turtle.tracer(3)
+
+class Sprite(turtle.Turtle):
+    def __init__(self, spriteshape, color, startx, starty):
+        turtle.Turtle.__init__(self, shape = spriteshape)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.fd(0)
+        self.goto(startx,starty)
+        self.speed = 1
+    
+    def move(self):
+        self.fd(self.speed)
+
+        #Boundary detection
+        if self.xcor() > 290:
+            self.setx(290)
+            self.rt(60)
+        
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
+
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)
+
+    def is_collision(self,other):
+        if (self.xcor() >= (other.xcor() - 20)) and \
+        (self.xcor() <= (other.xcor() + 20)) and \
+        (self.ycor() >= (other.ycor() - 20)) and \
+        (self.ycor() <= (other.ycor() + 20)):
+            return True
+        else:
+            return False
+
+
+
+
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.6, stretch_len=1.1, outline=None)        
+        self.speed = 4
+        self.lives = 3
+
+    def turn_left(self):
+        self.lt(45)
+
+    def turn_right(self):
+        self.rt(45)
+    
+    def accelerate(self):
+        self.speed += 1
+    
+    def decelerate(self):
+        self.speed -= 1
+
+class Enemy(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 6
+        self.setheading(random.randint(0,360))
+
+class Ally(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 8
+        self.setheading(random.randint(0,360))
+    
+    def move(self):
+        self.fd(self.speed)
+        #Boundary detection
+        if self.xcor() > 290:
+            self.setx(290)
+            self.lt(60)
+        
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.lt(60)
+
+        if self.ycor() > 290:
+            self.sety(290)
+            self.lt(60)
+
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.lt(60)
+
+
+class Missile(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.speed = 20
+        self.status = "ready"
+        # self.goto(-1000, 1000)
+
+    def fire(self):
+        if self.status == "ready":
+            
+            self.goto(player.xcor(),player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+    
+    def move(self):
+        
+        if self.status == "ready":
+            self.goto(-1000, 1000)            
+        
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        #Border check
+        if self.xcor() < -290 or self.xcor() > 290 or self.ycor() <-290 or self.ycor() > 290:
+            self.goto(-1000, 1000)
+            self.status = "ready"
+
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+    def draw_border(self):
+        #Draw border
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300, 300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+        self.pen.pendown()
+    
+    def show_status(self):
+        self.pen.undo()
+        msg = "Score: %s" %(self.score)
+        self.pen.penup()
+        self.pen.goto(-300, 310)
+        self.pen.write(msg, font=("Arial", 16, "normal"))
+
+#Create game object
+game = Game() 
+
+#Draw game border
+game.draw_border()
+
+#show game status
+game.show_status()
+
+#Creat the sprites
+player = Player("triangle", "white", 0, 0)
+missile = Missile("triangle", "yellow", 0 , 0)
+
+
+enemies = []
+for i in range(5):
+    enemies.append(Enemy("circle", "red", -100, 0))
+
+
+allies = []
+for i in range(5):
+    allies.append(Ally("square", "blue", 100, 0))
+
+
+
+#keyboard bindings
+turtle.onkey(player.turn_left, "Left")
+turtle.onkey(player.turn_right, "Right")
+turtle.onkey(player.accelerate, "Up")
+turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
+turtle.listen()
+
+
+
+
+#Main game loop
+while True:
+    turtle.update()
+    time.sleep(0.02)
+
+
+    player.move()
+    missile.move()
+
+    for enemy in enemies:
+        enemy.move()
+        if player.is_collision(enemy):
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250)
+            enemy.goto(x,y)
+            game.score -= 100
+            game.show_status()
+
+        if missile.is_collision(enemy):
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250)
+            enemy.goto(x,y)
+            missile.status="ready"
+            game.score += 100
+            game.show_status()
+
+    for ally in allies:
+        ally.move()
+        if missile.is_collision(ally):
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250)
+            ally.goto(x,y)
+            missile.status="ready"    
+            game.score -= 50
+            game.show_status()
+        
+    
+
+
+
+
+delay = input("Press enter for finish. > ")
